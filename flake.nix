e413974421f1516ff6fff171eb8a70ac8af6be33
Lucian Ursu <lucian.ursu@gmail.com>
2023-06-13 20:13:51 +0300

    Add flake.nix with formatter and devshell
    Untested

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/22.11";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, config, nixpkgs, treefmt-nix }:
+    let
+      formatterOptions = {
+        projectRootFile = "flake.nix";
+        programs.nixpkgs-fmt.enable = true;
+        programs.terraform.enable = true;
+      };
+      linux-pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      darwin-pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      shellOptions = {
+        nativeBuildInputs = config.treefmt.build.programs;
+      };
+    in
+      {
+        formatter = {
+          x86_64-linux = treefmt-nix.lib.mkWrapper linux-pkgs formatterOptions;
+          aarch64-darwin = treefmt-nix.lib.mkWrapper darwin-pkgs formatterOptions;
+        };
+        devShell = {
+          x86_64-linux = linux-pkgs.mkShell shellOptions;
+          aarch64-darwin = darwin-pkgs.mkShell shellOptions;
+        };
+      };
+}
