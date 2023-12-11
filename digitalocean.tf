f84e988430bfe2e253f92b76fba82bff6ab972e9
Lucian Ursu <lucian.ursu@gmail.com>
2023-12-11 15:14:29 +0200

    Add mail dns records

--- digitalocean.tf
+++ digitalocean.tf
@@ -5,6 +5,8 @@ resource "digitalocean_domain" "elbear-com" {
   name = "elbear.com"
 }
 
+
+## Subdomains
 resource "digitalocean_record" "know_elbear_com" {
   domain = digitalocean_domain.elbear-com.name
   type   = "A"
@@ -93,6 +95,51 @@ resource "digitalocean_record" "sim_staging_elbear_com" {
   value  = oci_core_instance.oci_main.public_ip
   ttl    = 300
 }
+
+
+## Mail
+resource "digitalocean_record" "mx01-icloud" {
+  domain   = digitalocean_domain.elbear-com.name
+  type     = "MX"
+  name     = "@"
+  priority = 10
+  value    = "mx01.mail.icloud.com."
+  ttl      = 14400
+}
+
+resource "digitalocean_record" "mx02-icloud" {
+  domain   = digitalocean_domain.elbear-com.name
+  type     = "MX"
+  name     = "@"
+  priority = 10
+  value    = "mx02.mail.icloud.com."
+  ttl      = 14400
+}
+
+resource "digitalocean_record" "apple-domain" {
+  domain = digitalocean_domain.elbear-com.name
+  type   = "TXT"
+  name   = "@"
+  value  = "apple-domain=1cLlJuqH09gUDHAE"
+  ttl    = 3600
+}
+
+resource "digitalocean_record" "spf1" {
+  domain = digitalocean_domain.elbear-com.name
+  type   = "TXT"
+  name   = "@"
+  value  = "v=spf1 include:icloud.com ~all"
+  ttl    = 3600
+}
+
+resource "digitalocean_record" "sig1" {
+  domain = digitalocean_domain.elbear-com.name
+  type   = "CNAME"
+  name   = "sig1._domainkey.elbear.com"
+  value  = "sig1.dkim.elbear.com.at.icloudmailadmin.com."
+  ttl    = 43200
+}
+
 
 ## Object Storage
 resource "digitalocean_spaces_bucket" "elb_raw_information_store" {
